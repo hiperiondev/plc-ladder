6bbea7b23d9e6bda81273bb28b8d32547c5a9d42
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-03-08 21:25:58 -0300

    Delete unused mem tests

--- common/include/mem.h
+++ common/include/mem.h
@@ -8,28 +8,16 @@
 #ifndef DEBUG_MEM_H_
 #define DEBUG_MEM_H_
 
+#include <stddef.h>
+
 #define MEM_ALIGN 8 // 32bits: 4 | 64bits: 8
 
-//#define DEBUG_MEM
-
-#ifdef DEBUG_MEM
-#define MEM_MALLOC(size, tag)         debug_malloc(size, tag)
-#define MEM_CALLOC(count, size, tag)  debug_calloc(count, size, tag)
-#define MEM_FREE(ptr, tag)            debug_free(ptr, tag)
-#else
 #define MEM_MALLOC(size, tag)         a_malloc(size)
 #define MEM_CALLOC(count, size, tag)  a_calloc(count, size)
 #define MEM_FREE(ptr, tag)            a_free(ptr)
-#endif
 
-#include <stddef.h>
-
-void*    debug_malloc (size_t size, char* mem_tag);
-void*    debug_calloc (size_t count, size_t size, char* mem_tag);
-void       debug_free (void *ptr, char* mem_tag);
-void   debug_free_all (void);
-void*        a_malloc (size_t size);
-void*        a_calloc (size_t count, size_t size);
-void           a_free (void *ptr);
+void* a_malloc (size_t size);
+void* a_calloc (size_t count, size_t size);
+void    a_free (void *ptr);
 
 #endif /* INCLUDE_MEM_H_ */
--- common/mem.c
+++ common/mem.c
@@ -12,19 +12,6 @@
 
 #include "mem.h"
 
-unsigned int totalAlloc = 0;
-void *ptrs[1000];
-char debug_mem_tags[1000][256];
-
-int ptr_cnt = 0;
-bool mem_init = false;
-
-static void debug_mem_init(void) {
-    for (int n = 0; n < 1000; n++)
-        ptrs[n] = 0;
-    mem_init = true;
-}
-
 void* a_malloc(size_t size) {
     void *ptr;
     void **aligned;
@@ -51,73 +38,4 @@ void* a_calloc(size_t count, size_t eltsize) {
 
 void a_free(void *ptr) {
     free(*(((void**) ptr) - 1));
-}
-
-void* debug_malloc(size_t size, char *mem_tag) {
-    if (!mem_init)
-        debug_mem_init();
-
-    void *p;
-    totalAlloc += size;
-
-    p = a_malloc(size + sizeof(int));
-
-    ptrs[ptr_cnt] = p;
-    strcpy(debug_mem_tags[ptr_cnt++], mem_tag);
-
-    printf("debug_malloc [%s (%d)] (total: %d)\n", mem_tag, ptr_cnt - 1, totalAlloc);
-    return p;
-}
-
-void* debug_calloc(size_t count, size_t size, char *mem_tag) {
-    if (!mem_init)
-        debug_mem_init();
-    void *p;
-    totalAlloc += size * count;
-
-    p = a_calloc(count, size + sizeof(int));
-
-    ptrs[ptr_cnt] = p;
-    strcpy(debug_mem_tags[ptr_cnt++], mem_tag);
-
-    printf("debug_calloc [%s (%d)] (total: %d)\n", mem_tag, ptr_cnt - 1, totalAlloc);
-    return p;
-}
-
-void debug_free(void *ptr, char *mem_tag) {
-    if (!mem_init)
-        debug_mem_init();
-
-    if (ptr == NULL) {
-        printf("debug_free [WARNING: pointer NULL (%s)] (total: %d/%d)\n", mem_tag, totalAlloc, ptr_cnt);
-        return;
-    }
-
-    int ptr_id = -1;
-    ptr = (void*) (((int*) ptr) - 1);
-    totalAlloc -= *(int*) ptr;
-    for (int n = 0; n < ptr_cnt; n++)
-        if (ptrs[n] == ptr) {
-            ptr_id = n;
-            break;
-        }
-
-    a_free(ptr);
-
-    if (ptr_id != -1)
-        ptrs[ptr_id] = 0;
-    printf("debug_free   [%s/%s (%d)] (total: %d/%d)\n", ptr_id == -1 ? "{unknown}" : debug_mem_tags[ptr_id], mem_tag, ptr_id, totalAlloc, ptr_cnt);
-}
-
-void debug_free_all(void) {
-    int cnt = 0;
-    for (int n = 0; n < 1000; n++)
-        if (ptrs[n] != 0) {
-            printf("debug_free_all: %s\n", debug_mem_tags[n]);
-            free(ptrs[n]);
-            ++cnt;
-        }
-    if (cnt != 0)
-        printf("WARNING!!! ");
-    printf("debug_free_all: %d\n", cnt);
 }
